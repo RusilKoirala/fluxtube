68b50a6f46150f6720d56b6ac2c564fe2a073f21
Rusil Koirala <rusilkoirala23@gmail.com>
2026-07-07 17:43:07 +0545

    feat: add new components and update styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>
-    <h1>Flux Tubee</h1>
+   <div className="flex">
+      <Image src="/logo.jpg" width={50} height={50} alt=""></Image>
+      <p className="text-xl font-semibold tracking-tight items-center mt-2"><span className="text-red-500">Flex</span>Tube</p>
    </div>
   );
 }
